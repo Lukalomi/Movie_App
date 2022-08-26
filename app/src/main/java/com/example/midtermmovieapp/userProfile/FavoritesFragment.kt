8f8a45a70fb0469ea8acd3d9906006de2d3e3799
Kito Kitoshvili <ivane.kitoshvili@gmail.com>
2022-08-26 20:12:19 +0400

    Add Favorites Fragment

---
+++ app/src/main/java/com/example/midtermmovieapp/userProfile/FavoritesFragment.kt
@@ -0,0 +1,32 @@
+package com.example.midtermmovieapp.userProfile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.midtermmovieapp.HomeViewModel
+import com.example.midtermmovieapp.databinding.FragmentHomeBinding
+
+
+class FavoritesFragment : Fragment() {
+
+    private lateinit var viewModel: HomeViewModel
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        return binding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding!!.appCompatImageButton.setOnClickListener {
+            openFavorites()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/midtermmovieapp/userProfile/FavoritesViewModel.kt
@@ -0,0 +1,5 @@
+package com.example.midtermmovieapp.userProfile
+
+class FavoritesViewModel {
+
+}
