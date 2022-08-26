0dfcc61ac2bc494c636700c0f70872cf425e4208
llomi13 <luka.lomidze00@gmail.com>
2022-08-26 23:34:51 +0400

    Changed minor thing in master branch by mistake

--- app/src/main/java/com/example/midtermmovieapp/userProfile/FavoritesFragment.kt
+++ app/src/main/java/com/example/midtermmovieapp/userProfile/FavoritesFragment.kt
@@ -5,28 +5,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.midtermmovieapp.HomeViewModel
+import com.example.midtermmovieapp.databinding.FragmentFavoritesBinding
 import com.example.midtermmovieapp.databinding.FragmentHomeBinding
 
 
 class FavoritesFragment : Fragment() {
 
     private lateinit var viewModel: HomeViewModel
-    private lateinit var binding: FragmentHomeBinding
+    private  var binding: FragmentFavoritesBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        binding = FragmentFavoritesBinding.inflate(inflater,container,false)
         return binding!!.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding!!.appCompatImageButton.setOnClickListener {
-            openFavorites()
-        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 }
