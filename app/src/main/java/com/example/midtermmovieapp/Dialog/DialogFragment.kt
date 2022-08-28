ebc46a3624f8b6d4d04e32ee160088055dbe37ec
llomi13 <luka.lomidze00@gmail.com>
2022-08-28 20:27:14 +0400

    Fixed Minor Issues. Project Is ready

--- app/src/main/java/com/example/midtermmovieapp/Dialog/DialogFragment.kt
+++ app/src/main/java/com/example/midtermmovieapp/Dialog/DialogFragment.kt
@@ -5,20 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-
 import com.bumptech.glide.Glide
-import com.example.midtermmovieapp.*
-
 import com.example.midtermmovieapp.databinding.FragmentDialogBinding
-
-import kotlinx.coroutines.launch
 
 class DialogFragment : Fragment() {
     private var binding: FragmentDialogBinding? = null
@@ -35,17 +25,28 @@ class DialogFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        fromDialogToHome()
+        setDialog()
 
+    }
+
+
+    private fun setDialog() {
 
         binding?.let {
             it.tvDialogMovieDesc.text = args.singleDialog?.overview
             it.tvDialogMovieName.text = args.singleDialog?.title
-            Glide.with(requireContext()).load("https://image.tmdb.org/t/p/w500"+args.singleDialog?.posterPath).into(it.ivDialogMovie)
+            Glide.with(requireContext())
+                .load("https://image.tmdb.org/t/p/w500" + args.singleDialog?.posterPath)
+                .into(it.ivDialogMovie)
         }
-
-
     }
 
+    private fun fromDialogToHome() {
+        binding!!.btnBackFromDialog.setOnClickListener {
+            findNavController().navigate(DialogFragmentDirections.actionDialogFragmentToHomeFragment())
+        }
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/example/midtermmovieapp/Dialog/TopRatedDialogFragment.kt
+++ app/src/main/java/com/example/midtermmovieapp/Dialog/TopRatedDialogFragment.kt
@@ -1,38 +1,52 @@
 package com.example.midtermmovieapp.Dialog
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.example.midtermmovieapp.databinding.FragmentTopRatedDialogBinding
 
 class TopRatedDialogFragment : Fragment() {
 
-   private var binding: FragmentTopRatedDialogBinding? = null
+    private var binding: FragmentTopRatedDialogBinding? = null
     private val args: TopRatedDialogFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentTopRatedDialogBinding.inflate(inflater,container,false)
+        binding = FragmentTopRatedDialogBinding.inflate(inflater, container, false)
         return binding!!.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setDialog()
+        fromDialogToHome()
 
+
+    }
+
+    private fun setDialog() {
         binding?.let {
             it.tvDialogMovieName.text = args.topRatedDialog?.title
             it.tvDialogMovieDesc.text = args.topRatedDialog?.overview
-            Glide.with(requireContext()).load("https://image.tmdb.org/t/p/w500" + args.topRatedDialog?.posterPath).into(it.ivDialogMovie)
+            Glide.with(requireContext())
+                .load("https://image.tmdb.org/t/p/w500" + args.topRatedDialog?.posterPath)
+                .into(it.ivDialogMovie)
         }
     }
 
+    private fun fromDialogToHome() {
+
+        binding!!.btnBackFromDialog.setOnClickListener {
+            findNavController().navigate(TopRatedDialogFragmentDirections.actionTopRatedDialogFragmentToHomeFragment())
+        }
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
